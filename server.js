9051d321a02946b0efde367f8a6e905149930628
Ramiro Di Carlo <ramirodicarlo1@gmail.com>
2021-04-19 19:33:20 -0300

    Finish put and post method and correct style.css bugs

--- server.js
+++ server.js
@@ -1,22 +1,36 @@
-const express = require('express')
-const app = express()
-const port = 4000
+const express = require('express');
+const cors = require('cors')
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.get('/', (req, res) => {
-  res.send("This is the server in port"+ port);
-  }) 
-app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`)
+    res.send('This is my port 4000')
+});
+
+app.listen(4000, () => {
+    console.log(`Server started`)
 })
 
-app.post('/register', (req, res) => {
-  res.json({ 
-    result:"Success"
-  })
-})
+app.post('/register', function (req, res) {
+  console.log(req.body)
+  const newUser = {
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.pass,
+  };
+  res.json({result:"Success"})
+});
 
-app.put('/login', (req, res) => {
-  res.json({ 
-    result:"Success"
-  })
-})
+app.put('/login', function (req, res) {
+  console.log(req.body)
+  const registeredUser = {
+      email: req.body.email,
+      pass: req.body.pass,}
+      
+  res.json({result:"Success"}) 
+});
+
