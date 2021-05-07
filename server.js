8beb61ec5bf68856a0c814e3a25750fa3f741078
Ramiro Di Carlo <ramirodicarlo1@gmail.com>
2021-05-07 00:59:55 -0300

    Corrected usseless lines

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send('This is my port 4000')
-});
+})
 
 app.listen(4000, () => {
     console.log(`Server started`)
@@ -17,26 +17,13 @@ app.listen(4000, () => {
 
 function handleLogin(req, res) {
   console.log(req.body)
-  const registeredUser = {
-      email: req.body.email,
-      pass: req.body.pass,}
-      
   res.json({result:"Success"}) 
 }
+
 function handleRegister (req, res) {
   console.log(req.body)
-  const newUser = {
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.pass,
-  };
   res.json({result:"Success"})
 }
+
 app.post('/register', handleRegister)
-app.get("/register", (req,res) =>{
-  res.send()
-
-  }
-)
-
 app.put('/login', handleLogin) 
